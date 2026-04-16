aaf994b7726190979ac66dfa835f0bdde9e87723
Carson Adams <cgadams42@tntech.edu>
2026-04-15 20:47:31 -0500

    please work

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 window.NBA_PLAYOFFS_CONFIG = {
-  firebaseConfig: {
-    apiKey: "REPLACE_ME",
-    authDomain: "REPLACE_ME.firebaseapp.com",
-    projectId: "REPLACE_ME",
-    storageBucket: "REPLACE_ME.firebasestorage.app",
-    messagingSenderId: "REPLACE_ME",
-    appId: "REPLACE_ME"
-  },
-  gameId: "2026-playoffs-challenge",
-  commissionerPin: "REPLACE_ME"
+firebaseConfig = {
+  apiKey: "AIzaSyDMG9XIlT7pHuQwYNohgnN3eywQt9G76SQ",
+  authDomain: "web-app-a6fe6.firebaseapp.com",
+  projectId: "web-app-a6fe6",
+  storageBucket: "web-app-a6fe6.firebasestorage.app",
+  messagingSenderId: "412298670703",
+  appId: "1:412298670703:web:eaab2d9cfc860dcc67d39a",
+  measurementId: "G-K56YVG24Q5"
+}
 };
